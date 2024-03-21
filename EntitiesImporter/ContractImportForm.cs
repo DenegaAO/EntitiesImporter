3d068010ce29dbe3b4cf237d2d367435c2da1e58
denega <aden@es.krasnoyarsk.ru>
2024-03-21 15:54:41 +0700

    Add project files.

---
+++ EntitiesImporter/ContractImportForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntitiesImporter
+{
+    public partial class ContractImportForm : System.Windows.Forms.Form
+    {
+        public ContractImportForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EntitiesImporter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntitiesImporter
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            string ServerName = args[1];
+            string BaseName = args[2];
+            string PodCast = args[3];
+
+            Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+            if (PodCast == "Contract") Application.Run(new ContractImportForm());
+            else MessageBox.Show("Функция " + PodCast + " не реализована", "Импорт сущностей в ПК OmniUS");
+		}
+	}
+}
